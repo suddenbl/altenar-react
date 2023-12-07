99a7cdc7c86322aff22ad31be12620b3fef788af
sorryfo <sorryfoff@gmail.com>
2023-12-07 22:47:05 +0300

    started to do creation-page

--- src/pages/Creation/Creation.tsx
+++ src/pages/Creation/Creation.tsx
@@ -1,6 +1,16 @@
-import { CreationBlock } from '../../components/CreationBlock/CreationBlock'
 import { CreationHeader } from '../../components/CreationHeader/CreationHeader'
-import { CreationInput, Section } from './CreationStyles'
+import {
+  FormInput,
+  FormInputDescription,
+  FormSpan,
+  FormTextArea,
+  Section,
+  SectionBlock,
+  SectionBlockDescription,
+  SectionBlockFolder,
+  SectionBlockTitle,
+  SectionForm,
+} from './CreationStyles'
 
 export const Creation = () => {
   return (
@@ -8,17 +18,30 @@ export const Creation = () => {
       <CreationHeader />
       <main>
         <Section>
-          <CreationBlock
-            title={'Расскажите о подборке'}
-            description={'Желательно сделать это кратко и ёмко'}></CreationBlock>
-          <CreationBlock
-            title={'Загрузите обложку'}
-            description={null}></CreationBlock>
-          <CreationBlock
-            title={'Выберите цвет'}
-            description={
-              'Лучше всего подойдет цвет преобладающий на обложке'
-            }></CreationBlock>
+          <SectionBlock>
+            <div>
+              <SectionBlockTitle>Расскажите о подборке</SectionBlockTitle>
+              <SectionBlockDescription>
+                Желательно сделать это кратко и ёмко
+              </SectionBlockDescription>
+            </div>
+            <SectionForm>
+              <FormSpan>
+                <FormInput type="text" placeholder="Дискриптор" />
+                <FormInputDescription>30</FormInputDescription>
+              </FormSpan>
+              <FormSpan>
+                <FormInput type="text" placeholder="Название" />
+                <FormInputDescription>50</FormInputDescription>
+              </FormSpan>
+              <FormTextArea
+                name="description"
+                cols={30}
+                rows={5}
+                placeholder="Описание"
+                value=""></FormTextArea>
+            </SectionForm>
+          </SectionBlock>
         </Section>
       </main>
     </>
--- src/pages/Creation/CreationStyles.ts
+++ src/pages/Creation/CreationStyles.ts
@@ -6,6 +6,79 @@ export const Section = styled.section`
 `
 
 export const CreationInput = styled.input`
-  /* width: 100%; */
+  width: 100%;
   padding: 10px 5px;
+`
+
+export const SectionBlock = styled.div`
+  display: flex;
+  width: 35%;
+  flex-direction: column;
+  /* gap: 10px; */
+  margin-bottom: 20px;
+`
+
+export const SectionBlockFolder = styled.div``
+
+export const SectionBlockTitle = styled.h4`
+  color: #000000;
+  margin-bottom: 5px;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 32px;
+`
+
+export const SectionBlockDescription = styled.p`
+  /* color: $text-secondary; */
+  color: rgba(0, 0, 0, 0.54);
+  margin-bottom: 23px;
+  /* font-size: $font-size-normal; */
+  font-size: 14px;
+  line-height: 20px;
+`
+
+export const SectionForm = styled.form`
+  display: flex;
+  margin-bottom: 35px;
+  flex-direction: column;
+  gap: 18px;
+`
+
+export const FormSpan = styled.span`
+  position: relative;
+  display: inline-block;
+  width: 100%;
+`
+
+export const FormInput = styled.input`
+  width: 94%;
+  padding: 17px 11px;
+  border: none;
+  /* border: 1px solid $grey-300; */
+  border: 1px solid #e0e0e0;
+  border-radius: 4px;
+  &:hover {
+    outline: 1px solid #2196f3;
+  }
+`
+
+export const FormTextArea = styled.textarea`
+  padding: 17px 11px;
+  border: none;
+  /* border: 1px solid $grey-300; */
+  border: 1px solid #e0e0e0;
+  border-radius: 4px;
+  resize: none;
+  &:hover {
+    outline: 1px solid #2196f3;
+  }
+`
+
+export const FormInputDescription = styled.span`
+  position: absolute;
+  top: 20px;
+  right: 17px;
+  color: rgba(0, 0, 0, 0.54);
+  /* color: $text-secondary; */
+  /* outline: 1px solid $primary-main; */
 `
